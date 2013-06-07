8724ae4832e685018caf41e02dbca9cfb3d489a1
Chris Torstenson <itunes.torstenson@gmail.com>
2013-06-07 12:33:53 -0700

    Add Rails Skeleton.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandspitDesigns::Application.config.secret_token = 'e57833a646cc0d6a62bd90fb3b524b33ed4c79412f5afbc6ba1d63742ca92f897ac9c0815a5df2e14ad9759b7e04fdbd766db478832962c0d2b9703d40c12c32'
