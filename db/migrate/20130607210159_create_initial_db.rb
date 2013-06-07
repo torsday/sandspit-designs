139a4113e9df69a7c0457c6e95d02b1270879c85
Chris Torstenson <itunes.torstenson@gmail.com>
2013-06-07 14:08:41 -0700

    create initial migration (apps, sample_photos)

---
+++ db/migrate/20130607210159_create_initial_db.rb
@@ -0,0 +1,24 @@
+class CreateInitialDb < ActiveRecord::Migration
+
+  def up
+
+    create_table :apps do |t|
+      t.string :name, :null => false
+      t.text :description, :null => false
+      t.string :app_store_url, :null => false
+      t.string :main_photo_url, :null => false
+    end
+
+    create_table :sample_photos do |t|
+      t.references :app, :null => false
+      t.string :url, :null => false
+    end
+
+  end
+
+  def down
+    drop_table :apps
+    drop_table :sample_photos
+  end
+
+end
